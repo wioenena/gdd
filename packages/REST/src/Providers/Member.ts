7e4d37c3305546719552d0012e5d9ca016f74973
wioenena <wioenena@gmail.com>
2024-08-14 09:20:37 +0300

    feat: created provider for member requests

---
+++ packages/REST/src/Providers/Member.ts
@@ -0,0 +1,54 @@
+import { API_URL } from "../constants.ts";
+import { BaseProvider } from "./Base.ts";
+
+export class MemberProvider extends BaseProvider {
+    async updateNickname(serverId: string, userId: string, nickname: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}/members/${userId}/nickname`,
+            {
+                method: "PUT",
+                body: JSON.stringify({ nickname }),
+            },
+        );
+
+        return response.json();
+    }
+
+    async deleteNickname(serverId: string, userId: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}/members/${userId}/nickname`,
+            {
+                method: "DELETE",
+            },
+        );
+
+        return response.ok;
+    }
+
+    async get(serverId: string, userId: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}/members/${userId}`,
+        );
+
+        return response.json();
+    }
+
+    async kick(serverId: string, userId: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}/members/${userId}`,
+            {
+                method: "DELETE",
+            },
+        );
+
+        return response.ok;
+    }
+
+    async members(serverId: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}/members`,
+        );
+
+        return response.json();
+    }
+}
