2cc85a4bee2555fc074e9f6b9b6edc96fdd67f64
wioenena <wioenena@gmail.com>
2024-08-14 08:53:23 +0300

    chore: return json data

--- packages/REST/src/Providers/Server.ts
+++ packages/REST/src/Providers/Server.ts
@@ -2,7 +2,10 @@ import { API_URL } from "../constants.ts";
 import { BaseProvider } from "./Base.ts";
 
 export class ServerProvider extends BaseProvider {
-    get(serverId: string) {
-        return this.rest.makeRequest(`${API_URL}/servers/${serverId}`);
+    async get(serverId: string) {
+        const response = await this.rest.makeRequest(
+            `${API_URL}/servers/${serverId}`,
+        );
+        return response.json();
     }
 }
