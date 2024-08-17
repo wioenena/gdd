7061d5a4982fba92563b005fb96ad10109c91ae8
wioenena <wioenena@gmail.com>
2024-08-17 11:16:50 +0300

    feat: ServerModel type defined

---
+++ packages/api-types/src/Models/Server.ts
@@ -0,0 +1,38 @@
+export type ServerModel = {
+    id: string;
+    ownerId: string;
+    type?: ServerModelType;
+    name: string;
+    url?: string;
+    about?: string;
+    avatar?: string;
+    banner?: string;
+    timezone?: string;
+    isVerified?: boolean;
+    /**
+     * The default channel id(uuid) for the server
+     */
+    defaultChannelId?: string;
+    createdAt: string;
+};
+
+export type ServerModelType =
+    | "team"
+    | "organization"
+    | "community"
+    | "clan"
+    | "guild"
+    | "friends"
+    | "streaming"
+    | "other";
+
+export const enum ServerType {
+    Team = "team",
+    Organization = "organization",
+    Community = "community",
+    Clan = "clan",
+    Guild = "guild",
+    Friends = "friends",
+    Streaming = "streaming",
+    Other = "other",
+}
