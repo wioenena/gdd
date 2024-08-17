1c039ce2c07fffca43b5d4698bb2b65b9d1636dd
wioenena <wioenena@gmail.com>
2024-08-17 11:38:52 +0300

    feat: GroupModel and request types defined

---
+++ packages/api-types/src/Models/Group.ts
@@ -0,0 +1,61 @@
+export type GroupModel = {
+    id: string;
+    serverId: string;
+    name: string;
+    description?: string;
+    avatar?: string;
+    isHome?: boolean;
+    emoteId?: number;
+    isPublic?: boolean;
+    createdAt: string;
+    createdBy: string;
+    updatedAt?: string;
+    updatedBy?: string;
+    archivedAt?: string;
+    archivedBy?: string;
+};
+
+export type CreateGroupPathParams = {
+    serverId: string;
+};
+
+export type CreateGroupJSONParams =
+    & Pick<GroupModel, "name">
+    & Partial<Pick<GroupModel, "description" | "emoteId" | "isPublic">>;
+
+export type CreateGroupResponseType = {
+    group: GroupModel;
+};
+
+export type GetGroupsPathParams = {
+    serverId: string;
+};
+
+export type GetGroupsResponseType = {
+    groups: GroupModel[];
+};
+
+export type GetGroupPathParams = {
+    serverId: string;
+    groupId: string;
+};
+
+export type GetGroupResponseType = {
+    group: GroupModel;
+};
+
+export type UpdateGroupPathParams = {
+    serverId: string;
+    groupId: string;
+};
+
+export type UpdateGroupJSONParams = Partial<
+    Pick<GroupModel, "name" | "description" | "emoteId" | "isPublic">
+>;
+
+export type UpdateGroupResponseType = { group: GroupModel };
+
+export type DeleteGroupPathParams = {
+    serverId: string;
+    groupId: string;
+};
